31ed41ad7f24d5b759e242b33681e9266911ca2b
Sebastian <u201611430@upc.edu.pe>
2024-09-24 14:45:16 -0500

    chore: Add base service and db.json.

---
+++ src/shared/services/base.service.js
@@ -0,0 +1,26 @@
+// src/services/base.service.js
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3000/';
+
+export const baseService = {
+    async get(endpoint) {
+        const response = await axios.get(${API_URL}${endpoint});
+        return response.data;
+    },
+
+    async post(endpoint, data) {
+        const response = await axios.post(${API_URL}${endpoint}, data);
+        return response.data;
+    },
+
+    async put(endpoint, data) {
+        const response = await axios.put(${API_URL}${endpoint}, data);
+        return response.data;
+    },
+
+    async delete(endpoint) {
+        const response = await axios.delete(${API_URL}${endpoint});
+        return response.data;
+    }
+};
