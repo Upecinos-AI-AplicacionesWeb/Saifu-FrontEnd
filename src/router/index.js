5620934fb20175697ad488e60c16e5394578f1fb
Johan Quiñones <91556525+johaanq@users.noreply.github.com>
2024-09-24 22:34:56 -0500

    feat:  add main-page.component.vue and profile.component.vue and routes.

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
+import MainPage from '../saifu/profile/components/main-page.component.vue';
+import Profile from '../saifu/profile/components/profile.component.vue';
+
+const routes = [
+    { path: '/', component: MainPage },
+    { path: '/profile', component: Profile }
+];
 
 const router = createRouter({
     history: createWebHistory(),
-    routes:[
-
-    ]
+    routes
 });
 
 export default router;
