541f21c25a45c7348938c3ce976baeae107c6864
Nato0211 <nato0211@gmail.com>
2024-09-25 02:36:19 -0500

    feature login: add Login and Register

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,14 @@
-<script setup>
-import Toolbar from './shared/components/toolbar.component.vue';
+<script>
+export default {
+  name: 'App',
+};
 </script>
 
 <template>
-  <Toolbar/>
-  <router-view/>
+  <router-view></router-view>
 </template>
 
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import MainPage from '../saifu/profile/components/main-page.component.vue';
 import Profile from '../saifu/profile/components/profile.component.vue';
 import ProfileEdit from '../saifu/profile/components/profile-edit.component.vue';
+import Login from "../saifu/loginRegister/Login.vue";
+
 
 const routes = [
     { path: '/', component: MainPage },
     { path: '/profile', component: Profile },
-    { path: '/profile-edit', component: ProfileEdit }
+    { path: '/profile-edit', component: ProfileEdit },
+    { path: '/login', component: Login}
 ];
 
 const router = createRouter({
