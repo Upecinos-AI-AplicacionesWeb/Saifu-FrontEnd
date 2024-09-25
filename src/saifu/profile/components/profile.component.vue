a901fa33361dbf82c44f13f27cf1506ada0d4644
Nato0211 <nato0211@gmail.com>
2024-09-25 04:19:27 -0500

    feature transaction: edit main page

--- src/saifu/profile/components/profile.component.vue
+++ src/saifu/profile/components/profile.component.vue
@@ -1,4 +1,7 @@
 ﻿<template>
+  <div>
+    <ToolbarComponent/>
+  </div>
   <div class="profile">
     <div class="profile-content" v-if="person">
       <div class="profile-header">
@@ -71,9 +74,12 @@
 import { ref, onMounted } from 'vue';
 import { personService } from '../services/person.service.js';
 import { useRouter } from 'vue-router';
-
+import ToolbarComponent from "../../../shared/components/toolbar.component.vue";
 export default {
   name: 'Profile',
+  components:{
+    ToolbarComponent
+  },
   setup() {
     const person = ref(null)
     const router = useRouter();
